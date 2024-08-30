d09bcb2895dd3dc78fc3288b94f130c5ec1874f8
Patrick Lee <pattyshack101@gmail.com>
2024-08-30 00:51:21 -0700

    raw lexer skeleton + lex constant symbols

---
+++ parser/raw_lexer.go
@@ -0,0 +1,274 @@
+package parser
+
+import (
+	//"fmt"
+	"io"
+	"unicode/utf8"
+
+	"github.com/pattyshack/gt/lexutil"
+	"github.com/pattyshack/gt/stringutil"
+)
+
+const (
+	lineCommentToken  = SymbolId(-1)
+	blockCommentToken = SymbolId(-2)
+)
+
+type RawLexerOptions struct {
+	PreserveCommentContent bool
+
+	InitialLookAheadBufferSize int
+}
+
+type RawLexer struct {
+	RawLexerOptions
+
+	lexutil.BufferedByteLocationReader
+	*stringutil.InternPool
+}
+
+func NewRawLexer(
+	sourceFileName string,
+	sourceContent io.Reader,
+	options RawLexerOptions,
+) RawLexer {
+	return RawLexer{
+		RawLexerOptions: options,
+		BufferedByteLocationReader: lexutil.NewBufferedByteLocationReader(
+			sourceFileName,
+			sourceContent,
+			options.InitialLookAheadBufferSize),
+		InternPool: stringutil.NewInternPool(),
+	}
+}
+
+// variable length token will return 0 as length
+func (lexer *RawLexer) peekNextToken() (SymbolId, int, error) {
+	peeked, err := lexer.Peek(7)
+	if len(peeked) > 0 && err == io.EOF {
+		err = nil
+	}
+	if err != nil {
+		return 0, 0, err
+	}
+
+	if len(peeked) == 0 {
+		panic("This should never happen")
+	}
+
+	char := peeked[0]
+
+	if ('a' <= char && char < 'r') ||
+		// note that 'r' could be a raw string literal
+		('r' < char && char <= 'z') ||
+		('A' <= char && char <= 'Z') ||
+		char == '_' {
+
+		return IdentifierToken, 0, nil
+	}
+
+	if '0' <= char && char <= '9' {
+		return IntegerLiteralToken, 0, nil // int or float
+	}
+
+	switch char {
+	case '`', '"':
+		return StringLiteralToken, 0, nil
+	case 'r':
+		if len(peeked) > 1 && (peeked[1] == '`' || peeked[1] == '"') {
+			return StringLiteralToken, 0, nil
+		}
+
+		return IdentifierToken, 0, nil
+	case '\'':
+		return RuneLiteralToken, 0, nil
+	case '\r', '\n':
+		return NewlinesToken, 0, nil
+	case ' ', '\t':
+		return SpacesToken, 0, nil
+	case '@':
+		return JumpLabelToken, 0, nil
+
+	case '{':
+		return LbraceToken, 1, nil
+	case '}':
+		return RbraceToken, 1, nil
+	case '(':
+		return LparenToken, 1, nil
+	case ')':
+		return RparenToken, 1, nil
+	case '[':
+		return LbracketToken, 1, nil
+	case ']':
+		return RbracketToken, 1, nil
+	case '.':
+		if len(peeked) > 2 && peeked[1] == '.' && peeked[2] == '.' {
+			return DotDotDotToken, 3, nil
+		}
+
+		if len(peeked) > 1 && '0' <= peeked[1] && peeked[1] <= '9' {
+			return FloatLiteralToken, 0, nil
+		}
+
+		return DotToken, 1, nil
+	case ',':
+		return CommaToken, 1, nil
+	case '?':
+		return QuestionToken, 1, nil
+	case ';':
+		return SemicolonToken, 1, nil
+	case ':':
+		return ColonToken, 1, nil
+	case '$':
+		if len(peeked) > 1 && peeked[1] == '[' {
+			return DollarLbracketToken, 2, nil
+		}
+	case '+':
+		if len(peeked) > 1 {
+			if peeked[1] == '+' {
+				return AddOneAssignToken, 2, nil
+
+			} else if peeked[1] == '=' {
+				return AddAssignToken, 2, nil
+			}
+		}
+
+		return AddToken, 1, nil
+	case '-':
+		if len(peeked) > 1 {
+			if peeked[1] == '-' {
+				return SubOneAssignToken, 2, nil
+
+			} else if peeked[1] == '=' {
+				return SubAssignToken, 2, nil
+			}
+		}
+
+		return SubToken, 1, nil
+	case '*':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return MulAssignToken, 2, nil
+		}
+
+		return MulToken, 1, nil
+	case '/':
+		if len(peeked) > 1 {
+			if peeked[1] == '/' {
+				return lineCommentToken, 0, nil
+			} else if peeked[1] == '*' {
+				return blockCommentToken, 0, nil
+			} else if peeked[1] == '=' {
+				return DivAssignToken, 2, nil
+			}
+		}
+
+		return DivToken, 1, nil
+	case '%':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return ModAssignToken, 2, nil
+		}
+
+		return ModToken, 1, nil
+	case '~':
+		if len(peeked) > 1 {
+			if peeked[1] == '=' {
+				return BitNegAssignToken, 2, nil
+
+			} else if peeked[1] == '~' {
+				return TildeTildeToken, 2, nil
+			}
+		}
+
+		return BitNegToken, 1, nil
+	case '&':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return BitAndAssignToken, 2, nil
+		}
+
+		return BitAndToken, 1, nil
+	case '^':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return BitXorAssignToken, 2, nil
+		}
+
+		return BitXorToken, 1, nil
+	case '|':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return BitOrAssignToken, 2, nil
+		}
+
+		return BitOrToken, 1, nil
+	case '=':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return EqualToken, 2, nil
+		}
+
+		return AssignToken, 1, nil
+	case '!':
+		if len(peeked) > 1 && peeked[1] == '=' {
+			return NotEqualToken, 2, nil
+		}
+
+		return ExclaimToken, 1, nil
+	case '<':
+		if len(peeked) > 1 {
+			if peeked[1] == '=' {
+				return LessOrEqualToken, 2, nil
+
+			} else if peeked[1] == '<' {
+				if len(peeked) > 2 && peeked[2] == '=' {
+					return BitLshiftAssignToken, 3, nil
+				}
+
+				return BitLshiftToken, 2, nil
+			}
+		}
+
+		return LessToken, 1, nil
+	case '>':
+		if len(peeked) > 1 {
+			if peeked[1] == '=' {
+				return GreaterOrEqualToken, 2, nil
+
+			} else if peeked[1] == '>' {
+				if len(peeked) > 2 && peeked[2] == '=' {
+					return BitRshiftAssignToken, 3, nil
+				}
+
+				return BitRshiftToken, 2, nil
+			}
+		}
+
+		return GreaterToken, 1, nil
+	}
+
+	utf8Char, size := utf8.DecodeRune(peeked)
+	if size == 1 || utf8Char == utf8.RuneError { // unexpected token
+		return LexErrorToken, size, nil
+	}
+
+	return IdentifierToken, 0, nil
+}
+
+func (lexer *RawLexer) Next() (Token, error) {
+	symbolId, size, err := lexer.peekNextToken()
+	if err != nil {
+		return nil, err
+	}
+
+	if symbolId == LexErrorToken {
+		// LexErrorToken
+		panic("TODO")
+	}
+
+	if size > 0 {
+		lexer.Discard(size)
+		return &GenericSymbol{
+			SymbolId: symbolId,
+			Location: Location(lexer.Location),
+		}, nil
+	}
+
+	// variable length tokens
+	panic("TODO")
+}
---
+++ parser/raw_lexer_test.go
@@ -0,0 +1,208 @@
+package parser
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/pattyshack/gt/testing/expect"
+	"github.com/pattyshack/gt/testing/suite"
+)
+
+type RawLexerSuite struct {
+}
+
+func TestRawLexer(t *testing.T) {
+	suite.RunTests(t, &RawLexerSuite{})
+}
+
+func (s *RawLexerSuite) lex(
+	t *testing.T,
+	input string,
+	expected ...SymbolId,
+) []Token {
+	buffer := bytes.NewBufferString(input)
+
+	lexer := NewRawLexer(
+		"source.txt",
+		buffer,
+		RawLexerOptions{})
+
+	tokens := []Token{}
+	tokenIds := []SymbolId{}
+	for {
+		token, err := lexer.Next()
+		if err == io.EOF {
+			break
+		}
+
+		expect.Nil(t, err)
+
+		tokens = append(tokens, token)
+		tokenIds = append(tokenIds, token.Id())
+	}
+
+	expect.Equal(t, expected, tokenIds)
+
+	return tokens
+}
+
+func (s *RawLexerSuite) TestIdentifierToken(t *testing.T) {
+	// TODO
+}
+
+func (s *RawLexerSuite) TestKeywordTokens(t *testing.T) {
+	// TODO
+}
+
+func (s *RawLexerSuite) TestIntegerLiteralToken(t *testing.T) {
+	// TODO (reminder: need to check for dot prefix variant .123)
+}
+
+func (s *RawLexerSuite) TestFloatLiteralToken(t *testing.T) {
+	// TODO (reminder: need to check for dot prefix variant .123)
+}
+
+func (s *RawLexerSuite) TestRuneLiteralToken(t *testing.T) {
+	// TODO
+}
+
+func (s *RawLexerSuite) TestStringLiteralToken(t *testing.T) {
+	// TODO 4 variants
+}
+
+func (s *RawLexerSuite) TestNewlinesTokens(t *testing.T) {
+	// TODO (also need to check lex error for '\r' without '\n')
+}
+
+func (s *RawLexerSuite) TestSpacesTokens(t *testing.T) {
+	// TODO
+}
+
+func (s *RawLexerSuite) TestAtTokens(t *testing.T) {
+	// TODO lex error for @ not followed by identifier
+	// TODO jump label
+}
+
+func (s *RawLexerSuite) TestLbraceTokens(t *testing.T) {
+	s.lex(t, "+{-", AddToken, LbraceToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestRbraceTokens(t *testing.T) {
+	s.lex(t, "+}-", AddToken, RbraceToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestLparenTokens(t *testing.T) {
+	s.lex(t, "+(-", AddToken, LparenToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestRparenTokens(t *testing.T) {
+	s.lex(t, "+)-", AddToken, RparenToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestLbracketTokens(t *testing.T) {
+	s.lex(t, "+[-", AddToken, LbracketToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestRbracketTokens(t *testing.T) {
+	s.lex(t, "+]-", AddToken, RbracketToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestDotTokens(t *testing.T) {
+	s.lex(t, "+.-", AddToken, DotToken, SubToken)
+	s.lex(t, "+...-", AddToken, DotDotDotToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestCommaTokens(t *testing.T) {
+	s.lex(t, "+,-", AddToken, CommaToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestQuestionTokens(t *testing.T) {
+	s.lex(t, "+?-", AddToken, QuestionToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestSemicolonTokens(t *testing.T) {
+	s.lex(t, "+;-", AddToken, SemicolonToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestColonTokens(t *testing.T) {
+	s.lex(t, "+:-", AddToken, ColonToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestDollarTokens(t *testing.T) {
+	// TODO lex error for $
+	s.lex(t, "+$[-", AddToken, DollarLbracketToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestAddTokens(t *testing.T) {
+	s.lex(t, "*+/", MulToken, AddToken, DivToken)
+	s.lex(t, "*++/", MulToken, AddOneAssignToken, DivToken)
+	s.lex(t, "*+=/", MulToken, AddAssignToken, DivToken)
+}
+
+func (s *RawLexerSuite) TestSubTokens(t *testing.T) {
+	s.lex(t, "*-/", MulToken, SubToken, DivToken)
+	s.lex(t, "*--/", MulToken, SubOneAssignToken, DivToken)
+	s.lex(t, "*-=/", MulToken, SubAssignToken, DivToken)
+}
+
+func (s *RawLexerSuite) TestMulTokens(t *testing.T) {
+	s.lex(t, "+*-", AddToken, MulToken, SubToken)
+	s.lex(t, "+*=-", AddToken, MulAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestDivTokens(t *testing.T) {
+	// TODO line and block comments
+	s.lex(t, "+/-", AddToken, DivToken, SubToken)
+	s.lex(t, "+/=-", AddToken, DivAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestModTokens(t *testing.T) {
+	s.lex(t, "+%-", AddToken, ModToken, SubToken)
+	s.lex(t, "+%=-", AddToken, ModAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestBitNegTokens(t *testing.T) {
+	s.lex(t, "+~-", AddToken, BitNegToken, SubToken)
+	s.lex(t, "+~=-", AddToken, BitNegAssignToken, SubToken)
+	s.lex(t, "+~~-", AddToken, TildeTildeToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestBitAndTokens(t *testing.T) {
+	s.lex(t, "+&-", AddToken, BitAndToken, SubToken)
+	s.lex(t, "+&=-", AddToken, BitAndAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestBitXorTokens(t *testing.T) {
+	s.lex(t, "+^-", AddToken, BitXorToken, SubToken)
+	s.lex(t, "+^=-", AddToken, BitXorAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestBitOrTokens(t *testing.T) {
+	s.lex(t, "+|-", AddToken, BitOrToken, SubToken)
+	s.lex(t, "+|=-", AddToken, BitOrAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestEqualTokens(t *testing.T) {
+	s.lex(t, "(=)", LparenToken, AssignToken, RparenToken)
+	s.lex(t, "(==)", LparenToken, EqualToken, RparenToken)
+}
+
+func (s *RawLexerSuite) TestExclaimTokens(t *testing.T) {
+	s.lex(t, "+!-", AddToken, ExclaimToken, SubToken)
+	s.lex(t, "+!=-", AddToken, NotEqualToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestGreaterTokens(t *testing.T) {
+	s.lex(t, "+>-", AddToken, GreaterToken, SubToken)
+	s.lex(t, "+>=-", AddToken, GreaterOrEqualToken, SubToken)
+	s.lex(t, "+>>-", AddToken, BitRshiftToken, SubToken)
+	s.lex(t, "+>>=-", AddToken, BitRshiftAssignToken, SubToken)
+}
+
+func (s *RawLexerSuite) TestLessTokens(t *testing.T) {
+	s.lex(t, "+<-", AddToken, LessToken, SubToken)
+	s.lex(t, "+<=-", AddToken, LessOrEqualToken, SubToken)
+	s.lex(t, "+<<-", AddToken, BitLshiftToken, SubToken)
+	s.lex(t, "+<<=-", AddToken, BitLshiftAssignToken, SubToken)
+}
