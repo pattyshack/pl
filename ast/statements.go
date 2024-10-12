c3052c454853405d196fe5e0734656eb34a9a0c4
Patrick Lee <pattyshack101@gmail.com>
2024-10-12 11:49:40 -0700

    add package id validation

--- ast/statements.go
+++ ast/statements.go
@@ -1,8 +1,11 @@
 package ast
 
 import (
-	"github.com/pattyshack/gt/lexutil"
+	"fmt"
+	"path"
 	"strings"
+
+	"github.com/pattyshack/gt/lexutil"
 )
 
 //
@@ -28,6 +31,39 @@ func (id PackageID) String() string {
 	}
 
 	return id.Path
+}
+
+func (id PackageID) Name() string {
+	return path.Base(id.Path)
+}
+
+func (id PackageID) Validate() error {
+	idStr := path.Clean(id.String())
+	if strings.HasPrefix(idStr, "/") ||
+		strings.HasPrefix(idStr, ".") ||
+		strings.HasPrefix(idStr, "@") {
+
+		return fmt.Errorf("invalid pacakge id: %s", idStr)
+	}
+
+	if idStr != id.String() {
+		return fmt.Errorf("invalid package id, path is not clean: %s", id.String())
+	}
+
+	pkgName := id.Name()
+	if !lexutil.IsValidIdentifier(pkgName) {
+		return fmt.Errorf(
+			"invalid package name (%v), package name must be a valid identifier",
+			pkgName)
+	}
+
+	if pkgName == "bin" {
+		return fmt.Errorf(
+			"invalid package name, %s is reserved",
+			pkgName)
+	}
+
+	return nil
 }
 
 type ImportClause struct {
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/pattyshack/pl
 
 go 1.22.6
 
-require github.com/pattyshack/gt v0.0.0-20241010210809-85e0abe69247
+require github.com/pattyshack/gt v0.0.0-20241012184358-8f18c6c6d37c
 
 require gopkg.in/yaml.v3 v3.0.1
